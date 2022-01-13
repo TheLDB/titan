1f581be14a98408bdb11b0bfc909c74616cbb4b9
Landon Boles <ldb@erikboles.com>
2022-01-12 19:46:10 -0700

    Its alive

--- src/helpers/get_collection.rs
+++ src/helpers/get_collection.rs
@@ -2,6 +2,7 @@ use serde_json::{Value};
 
 #[derive(Debug)]
 pub struct Collection {
+    pub status: reqwest::StatusCode,
     pub name: String,
     pub fp: Value,
     pub stats: Value,
@@ -10,12 +11,12 @@ pub struct Collection {
 impl Collection {
     pub async fn get(collection_slug: &str) -> Result<Collection, Box<dyn std::error::Error>>   {
         let resp = reqwest::get(format!("https://api.opensea.io/api/v1/collection/{}", collection_slug)).await?;
-        println!("Status: {}", resp.status());
+        let status = resp.status();
         
         let body = resp.text().await?;
         let v: Value = serde_json::from_str(&body)?;
-        // println!("{}", v);
         let new = Collection {
+            status: status,
             name: v["collection"]["name"].to_string(),
             fp: v["collection"]["stats"]["floor_price"].clone(),
             stats: v["collection"]["stats"].clone(),
--- src/helpers/slash_commands.rs
+++ src/helpers/slash_commands.rs
@@ -21,46 +21,102 @@ use serenity::{
 };
 
 pub struct Handler;
+use crate::helpers;
 
+pub struct returnable {
+    exists: Option<bool>,
+    collection: Option<helpers::get_collection::Collection>,
+}
 #[async_trait]
 impl EventHandler for Handler {
     async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
         if let Interaction::ApplicationCommand(command) = interaction {
             let content = match command.data.name.as_str() {
-                // Define commands?
-                "ping" => "Hey, I'm alive!".to_string(),
-                "id" => {
+                "collection" => {
                     let options = command
-                        .data
-                        .options
-                        .get(0)
-                        .expect("Expected user option")
-                        .resolved
-                        .as_ref()
-                        .expect("Expected user object");
+                    .data
+                    .options
+                    .get(0)
+                    .expect("Expected collection slug")
+                    .resolved
+                    .as_ref()
+                    .expect("Expected collection slug?");
 
-                    if let ApplicationCommandInteractionDataOptionValue::User(user, _member) =
-                        options
-                    {
-                        format!("{}'s id is {}", user.tag(), user.id)
-                    } else {
-                        "Please provide a valid user".to_string()
+                if let ApplicationCommandInteractionDataOptionValue::String(slug) = options {
+                    let result = helpers::get_collection::Collection::get(slug).await;
+                    // let unwr_res = result.unwrap();
+                    if let Ok(unwr_res) = result {
+                        if unwr_res.status == 200 {
+                            returnable {
+                                exists: Some(true),
+                                collection: Some(unwr_res)
+                            }
+                        }
+                        else {
+                            returnable {
+                                exists: Some(false),
+                                collection: None,
+                            }
+                        }
                     }
+                    else {
+                        returnable {
+                            exists: Some(false),
+                            collection: None,
+                        }
+                    }
+                    
+                }
+                else {
+                    returnable {
+                        exists: Some(false),
+                        collection: None,
+                    }
+                }
                 },
                 // if no match
-                _ => "not implemented :(".to_string(),
+                _ => returnable {
+                    exists: Some(false),
+                    collection: None,
+                },
             };
 
-            if let Err(why) = command
-                .create_interaction_response(&ctx.http, |response| {
-                    response
-                        .kind(InteractionResponseType::ChannelMessageWithSource)
-                        .interaction_response_data(|message| message.content(content))
-                })
-                .await
-            {
-                println!("Cannot respond to slash command: {}", why);
-            }
+            match command.data.name.as_str() {
+                "collection" => {
+                    if let Err(why) = command
+                        .create_interaction_response(&ctx.http, |response| {
+                            response
+                                .kind(InteractionResponseType::ChannelMessageWithSource)
+                                .interaction_response_data(|message| {
+                                    if let Some(or) = content.collection {
+                                        message.content(or.fp)
+                                    }
+                                    else {
+                                        message.content("Couldn't find collection!")
+                                    }
+                                })
+                        }).await {
+                            println!("Cannot respond to slash command: {}", why);
+                        }
+                },
+                "global" => {
+                    if let Err(why) = command
+                    .create_interaction_response(&ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| {
+                                println!("we hit!");
+                                message.content("we hit!")
+                            })
+                    }).await {
+                        println!("Cannot respond to slash command: {}", why);
+                    }
+                }
+                &_ => ()
+            };
+        }
+        else {
+            println!("wtf")
         }
     }
 
@@ -74,83 +130,36 @@ impl EventHandler for Handler {
                 .expect("GUILD_ID must be an integer"),
         );
 
-        let commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
+        let _commands = GuildId::set_application_commands(&guild_id, &ctx.http, |commands| {
             commands
             // Set commands with info abt them?
                 .create_application_command(|command| {
-                    command.name("ping").description("A ping command")
-                })
-                .create_application_command(|command| {
-                    command.name("id").description("Get a user id").create_option(|option| {
+                    command.name("collection").description("Get information about an OpenSea Collection").create_option(|option| {
                         option
-                            .name("id")
-                            .description("The user to lookup")
-                            .kind(ApplicationCommandOptionType::User)
+                            .name("slug")
+                            .description("The collection slug to fetch")
+                            .kind(ApplicationCommandOptionType::String)
                             .required(true)
                     })
                 })
-                .create_application_command(|command| {
-                    command
-                        .name("welcome")
-                        .description("Welcome a user")
-                        .create_option(|option| {
-                            option
-                                .name("user")
-                                .description("The user to welcome")
-                                .kind(ApplicationCommandOptionType::User)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("message")
-                                .description("The message to send")
-                                .kind(ApplicationCommandOptionType::String)
-                                .required(true)
-                                .add_string_choice(
-                                    "Welcome to our cool server! Ask me if you need help",
-                                    "pizza",
-                                )
-                                .add_string_choice("Hey, do you want a coffee?", "coffee")
-                                .add_string_choice(
-                                    "Welcome to the club, you're now a good person. Well, I hope.",
-                                    "club",
-                                )
-                                .add_string_choice(
-                                    "I hope that you brought a controller to play together!",
-                                    "game",
-                                )
-                        })
-                })
-                .create_application_command(|command| {
-                    command
-                        .name("numberinput")
-                        .description("Test command for number input")
-                        .create_option(|option| {
-                            option
-                                .name("int")
-                                .description("An integer from 5 to 10")
-                                .kind(ApplicationCommandOptionType::Integer)
-                                .required(true)
-                        })
-                        .create_option(|option| {
-                            option
-                                .name("number")
-                                .description("A float from -3.3 to 234.5")
-                                .kind(ApplicationCommandOptionType::Number)
-                                .required(true)
-                        })
-                })
+
         })
         .await;
 
-        println!("I now have the following guild slash commands: {:#?}", commands);
+        println!("I now have the following guild slash commands: {:#?}", _commands);
 
-        let guild_command =
+        let _guild_command =
             ApplicationCommand::create_global_application_command(&ctx.http, |command| {
-                command.name("wonderful_command").description("An amazing command")
+                command.name("collection-global").description("Get information about an OpenSea Collection").create_option(|option| {
+                    option
+                        .name("slug")
+                        .description("The collection slug to fetch")
+                        .kind(ApplicationCommandOptionType::String)
+                        .required(true)
+                })
             })
             .await;
 
-        println!("I created the following global slash command: {:#?}", guild_command);
+        // println!("I created the following global slash command: {:#?}", guild_command);
     }
 }
