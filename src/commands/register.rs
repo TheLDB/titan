2fe250d7d6b21c773bce6dcd9e2d1abacc374793
Landon Boles <ldb@erikboles.com>
2022-01-15 17:53:19 -0700

    Removed unneeded permissions

--- src/commands/register.rs
+++ src/commands/register.rs
@@ -2,7 +2,7 @@ type Data = ();
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
 
-#[poise::command(prefix_command, hide_in_help, required_permissions="ADMINISTRATOR")]
+#[poise::command(prefix_command, hide_in_help)]
 pub async fn register(ctx: Context<'_>) -> Result<(), Error> {
     poise::builtins::register_application_commands(ctx, false).await?;
     println!("registering");
